6115cc6ad36f542b8ebe535aa2a82778c7791a0e
adisharma995 <aditya2463sharama@gamil.com>
2025-03-30 16:25:00 +0530

    add feature

---
+++ ArithmeticProgression.java
@@ -0,0 +1,12 @@
+package revision1.adityaproject1;
+
+public class ArithmeticProgression {
+
+    public static void main(String[] args) {
+        
+        int a = 20;
+        int b  = 2;
+        int c  =  a +b;
+        System.out.println(c);
+    }
+}
